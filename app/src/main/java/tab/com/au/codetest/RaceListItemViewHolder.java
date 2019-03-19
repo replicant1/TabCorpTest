76e0184e2b00662a5f1dd202e41be739a5990813
rodbailey <rodney.p.bailey@gmail.com>
2019-03-19 19:16:56 +1100

    Race name appears in title case.

--- app/src/main/java/tab/com/au/codetest/RaceListItemViewHolder.java
+++ app/src/main/java/tab/com/au/codetest/RaceListItemViewHolder.java
@@ -1,22 +1,24 @@
 package tab.com.au.codetest;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.StringTokenizer;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import tab.com.au.codetest.data.Race;
 
 /**
- * Created by rodbailey on 19/3/19.
+ * View holder for a single list item in the Race list.
  */
-
 public class RaceListItemViewHolder extends RecyclerView.ViewHolder {
-
-	private static final String LOG_TAG = RaceListItemViewHolder.class.getSimpleName();
 
 	@BindView(R.id.tv_race_name)
 	TextView raceNameTextView;
@@ -29,20 +31,22 @@ public class RaceListItemViewHolder extends RecyclerView.ViewHolder {
 
 	public RaceListItemViewHolder(View itemView) {
 		super(itemView);
-		ButterKnife.bind(this,itemView);
+		ButterKnife.bind(this, itemView);
 	}
 
 	public void bind(Race race) {
-		Log.d(LOG_TAG, "bind: race=" + race);
-//		raceNameTextView = itemView.findViewById(R.id.tv_race_name);
-//		 raceTimeTextView = itemView.findViewById(R.id.tv_race_time);
-//		ImageView raceImageView = itemView.findViewById(R.id.iv_race_thumbnail);
+		// Title = "Race Name (race number)"
+		String formattedTitle = raceNameTextView.getContext().getString(R.string.race_name_number,
+				race.getRaceName(), race.getNumber());
+		raceNameTextView.setText(capitalize(formattedTitle));
 
-		// TODO: Get from formatted string
-		raceNameTextView.setText(race.getRaceName() + " (" + race.getNumber() + ")");
+		// Race time like "7:08 AM"
+		DateTime startDateTime = DateTime.parse(race.getRaceStartTime().toString());
+		DateTimeFormatter formatter = DateTimeFormat.shortTime();
+		String str = formatter.print(startDateTime);
+		raceTimeTextView.setText(str);
 
-		// TODO: Ignore date, show time in am.pm, change font color per current time.
-		raceTimeTextView.setText(race.getRaceStartTime().toString());
+		// Icon = race type
 		switch (race.getMeeting().getRaceType()) {
 			case HARNESS:
 				raceImageView.setImageResource(R.mipmap.ic_harness);
@@ -61,60 +65,21 @@ public class RaceListItemViewHolder extends RecyclerView.ViewHolder {
 		}
 	}
 
-	/**
-	 * Note this view holder class must not be an "inner" class. It must not have the implicit enclosing
-	 * instance that an "inner" class has. Omitting "inner" makes this Kotlin class the same as a nested
-	 * static class in Java.
-	 */
-//	class NewsAssetListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//
-//		@BindView(R.id.tv_news_asset_headline)
-//		lateinit var headlineTextView: TextView
-//
-//		@BindView(R.id.tv_news_asset_byline)
-//		lateinit var bylineTextView: TextView
-//
-//		@BindView(R.id.tv_news_asset_abstract)
-//		lateinit var abstractTextView: TextView
-//
-//		@BindView(R.id.iv_news_asset_thumbnail)
-//		lateinit var thumbnailImageView: ImageView
-//
-//		init {
-//			ButterKnife.bind(this, itemView)
-//		}
-//
-//		fun bind(item: NewsAssetDAO, clickListener: INewsAssetListItemClickListener) {
-//			// Copy item data into text fields for headline, byline and abstract
-//			headlineTextView.text = item.headline
-//			bylineTextView.text = item.byline
-//			abstractTextView.text = item.abstract
-//
-//			// Set click listener on this news asset - when clicked the user is transitioned to another
-//					// screen that shows the full news article inside a web view.
-//					itemView.setOnClickListener { clickListener.onNewsAssetClick(item) }
-//
-//			// TODO: Clear any current loading even if there is no replacement thumbnail
-//			val potentialThumbnails = item.relatedImages
-//			if (potentialThumbnails != null) {
-//				val thumbnail = SimpleThumbnailSelectionStrategy().selectThumbnail(potentialThumbnails)
-//
-//				// Cancel any outstanding Glide loads into the image view, then start a load of the thumbnail image
-//				// for the newly bound item. 60dp to pixels.
-//				val thumbnailHeightPx = itemView.context.resources.getDimension(R.dimen.asset_list_thumbnail_image_height).toInt()
-//				val thumbnailWidthPx = itemView.context.resources.getDimension(R.dimen.asset_list_thumbnail_image_width).toInt()
-//
-//				Timber.d("Loading thumbnail for item ${item.headline} which is at url ${thumbnail?.url}, " +
-//						"width=${thumbnail?.width}, height=${thumbnail?.height}")
-//
-//				GlideApp.with(itemView).clear(thumbnailImageView)
-//				GlideApp.with(itemView)
-//						.load(thumbnail?.url)
-//						.centerCrop()
-//						.override(thumbnailWidthPx, thumbnailHeightPx)
-//						.diskCacheStrategy(DiskCacheStrategy.ALL)
-//						.into(thumbnailImageView)
-//
-//			}
-//		}
+	private String capitalize(String str) {
+		StringTokenizer tokenizer = new StringTokenizer(str, " ");
+		StringBuffer result = new StringBuffer();
+
+		while (tokenizer.hasMoreTokens()) {
+			result.append(firstLetterToUpperCase(tokenizer.nextToken()));
+			result.append(" ");
+		}
+
+		return result.toString().trim();
+	}
+
+	private String firstLetterToUpperCase(String word) {
+		Character firstLetterUpperCase = Character.toUpperCase(word.charAt(0));
+		String remainderLowerCase = word.subSequence(1, word.length()).toString().toLowerCase();
+		return firstLetterUpperCase + remainderLowerCase;
+	}
 }
